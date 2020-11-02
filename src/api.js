c67b2cab5f5b20de050b4b4b93793fb35a6eefdf
EzequielCaballero <ezequiel.caballero33@gmail.com>
2020-11-01 23:15:13 -0300

    Adding CORS

--- src/api.js
+++ src/api.js
@@ -1,10 +1,12 @@
 'use strict';
 const express = require('express');
+const cors = require('cors');
 const routes = require('./routes');
 const serverless = require('serverless-http');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use('/.netlify/functions/api', routes);
